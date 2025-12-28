922b12516dacb2f1b713a5ee2cd3fbc3c2a73c9e
Simon Vilášek <simi.vilasek@gmail.com>
2025-12-28 11:20:34 +0100

    Delete programy-a-data directory

--- programy-a-data/b/main.js
+++
@@ -1,8 +0,0 @@
-const links = document.querySelectorAll("nav a");
-const currentPage = location.pathname.split("/").pop();
-
-links.forEach(link => {
-  if (link.getAttribute("href") === currentPage) {
-    link.classList.add("active");
-  }
-});
